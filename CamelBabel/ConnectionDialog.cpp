c59e27c4fa07a54ff90687bdeb7b91ffa81087b8
zanchi-r <romainzanchi30@gmail.com>
2013-12-11 11:52:32 +0100

    ConnectionDialog

--- CamelBabel/ConnectionDialog.cpp
+++ CamelBabel/ConnectionDialog.cpp
@@ -1,6 +1,14 @@
 #include "ConnectionDialog.hpp"
+#include "ui_ConnectionDialog.h"
 
 ConnectionDialog::ConnectionDialog(QWidget *parent) :
-  QDialog(parent)
+  QDialog(parent),
+  _ui(new Ui::ConnectionDialog)
 {
+  _ui->setupUi(this);
+}
+
+ConnectionDialog::~ConnectionDialog()
+{
+  delete _ui;
 }
