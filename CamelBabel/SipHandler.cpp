0ecd58ca3a33d55a1f73797193341d7ffd7439c7
post-l <ludovic.post@epitech.eu>
2014-01-06 23:08:48 +0100

    Fix contact selection signal/slot and some unused method/debug

--- CamelBabel/SipHandler.cpp
+++ CamelBabel/SipHandler.cpp
@@ -102,7 +102,6 @@ void SipHandler::readData()
   while (_socket->canReadLine())
     {
       line = _socket->readLine();
-      qDebug() << "Recv line: "<< line;
       if (!line.isEmpty())
         {
           line.resize(line.length() - 1);
@@ -136,11 +135,6 @@ void SipHandler::readData()
 						     stringList[1] == SIP_NOTFOUND ||
 						     stringList[1] == SIP_ALREADYCONNECTED))
 		handleError(stringList);
-	      else
-		{
-		  qDebug() << stringList;
-		  qDebug() << "No matching";
-		}
 	    }
         }
     }
